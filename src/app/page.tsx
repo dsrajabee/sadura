4e71d98171babfe94faa54c3a41b240d3c2fd4e3
Raja <dsarojraja@gmail.com>
2024-09-03 12:43:34 +0530

    Responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,12 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex  h-screen justify-center items-center">
-      <h1 className=" text-9xl font-bold uppercase flex flex-col text-right text-yellow-500">
-        Sadura <span className="text-4xl text-white">Software</span>
+    <main className="flex h-screen justify-center items-center p-4">
+      <h1 className="text-5xl sm:text-7xl md:text-9xl font-bold uppercase flex flex-col text-center sm:text-right text-yellow-500">
+        Sadura{' '}
+        <span className="text-2xl sm:text-3xl md:text-4xl text-white">
+          Software
+        </span>
       </h1>
     </main>
   );
