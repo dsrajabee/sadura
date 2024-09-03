b84aca396036faed039750ee6bb32b74f13fca18
Raja <dsarojraja@gmail.com>
2024-09-03 12:31:26 +0530

    Intro Color change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className="flex  h-screen justify-center items-center">
-      <h1 className=" text-9xl font-bold uppercase flex flex-col text-right ">
-        Sadura <span className="text-4xl ">Software</span>
+      <h1 className=" text-9xl font-bold uppercase flex flex-col text-right text-yellow-500">
+        Sadura <span className="text-4xl text-white">Software</span>
       </h1>
     </main>
   );
